e6fe1d5b7297826b3d7cf36a19aa930aa1396bfb
SPK137 <o.sippakorn@gmail.com>
2023-05-16 08:39:08 +0700

    feat: update installer for macOS

--- src/mobsf-starter/install-mob-dynamic-dep-command.cr
+++ src/mobsf-starter/install-mob-dynamic-dep-command.cr
@@ -24,52 +24,103 @@ module MobSFStarter
       android_cl_mac_install_loc = "/Library/Android/sdk"
       android_cl_linux_install_loc = "/usr/local/android/sdk"
 
-      STDOUT.puts "------------------------------------"
-      STDOUT.puts "Fetching required packages..."
-      STDOUT.puts "------------------------------------"
+      {% if flag?(:darwin) %}
+        STDOUT.puts "------------------------------------"
+        STDOUT.puts "Fetching required packages..."
+        STDOUT.puts "------------------------------------"
 
-      fetchZipStatus = Process.run("sudo apt-get -y install zip unzip curl openjdk-11-jdk adb", shell: true, input: STDIN, output: STDOUT, error: STDERR)
-      raise Exception.new("Error installing MobSF depedencies : zip") unless fetchZipStatus.success?
+        fetchZipStatus = Process.run("sudo brew -y install zip unzip curl openjdk@11", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : zip") unless fetchZipStatus.success?
 
-      STDOUT.puts "------------------------------------"
-      STDOUT.puts "Setup Android Command Line Tool..."
-      STDOUT.puts "------------------------------------"
+        STDOUT.puts "------------------------------------"
+        STDOUT.puts "Setup Android Command Line Tool..."
+        STDOUT.puts "------------------------------------"
 
-      downloadAndroidCLStatus = Process.run("curl #{android_cl_linux_url} --output android_cl.zip", shell: true, input: STDIN, output: STDOUT, error: STDERR)
-      raise Exception.new("Error installing MobSF depedencies : Download Android Command Line Tool") unless downloadAndroidCLStatus.success?
+        downloadAndroidCLStatus = Process.run("curl #{android_cl_mac_url} --output android_cl.zip", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : Download Android Command Line Tool") unless downloadAndroidCLStatus.success?
 
-      unzipAndroidCLStatus = Process.run("unzip android_cl.zip && sudo rm -rf #{android_cl_linux_install_loc}/cmdline-tools/ && sudo mkdir --parents #{android_cl_linux_install_loc}/cmdline-tools/ && mv cmdline-tools tools && sudo mv tools #{android_cl_linux_install_loc}/cmdline-tools/", shell: true, input: STDIN, output: STDOUT, error: STDERR)
-      raise Exception.new("Error installing MobSF depedencies : Unzipping Android Command Line Tool") unless unzipAndroidCLStatus.success?
+        unzipAndroidCLStatus = Process.run("unzip android_cl.zip && sudo rm -rf #{android_cl_mac_install_loc}/cmdline-tools/ && sudo mkdir --parents #{android_cl_mac_install_loc}/cmdline-tools/ && mv cmdline-tools tools && sudo mv tools #{android_cl_mac_install_loc}/cmdline-tools/", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : Unzipping Android Command Line Tool") unless unzipAndroidCLStatus.success?
 
-      Process.run("rm android_cl.zip", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        Process.run("rm android_cl.zip", shell: true, input: STDIN, output: STDOUT, error: STDERR)
 
-      downloadSDKDepStatus = Process.run("sudo #{android_cl_linux_install_loc}/cmdline-tools/tools/bin/sdkmanager platform-tools emulator", shell: true, input: STDIN, output: STDOUT, error: STDERR)
-      raise Exception.new("Error installing MobSF depedencies : Downloading platform-tools and emulator") unless downloadSDKDepStatus.success?
+        downloadSDKDepStatus = Process.run("sudo #{android_cl_mac_install_loc}/cmdline-tools/tools/bin/sdkmanager platform-tools emulator", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : Downloading platform-tools and emulator") unless downloadSDKDepStatus.success?
 
-      STDOUT.puts "----------------------------------------"
-      STDOUT.puts "Setting Environment Variables"
-      STDOUT.puts "----------------------------------------"
+        STDOUT.puts "----------------------------------------"
+        STDOUT.puts "Setting Environment Variables"
+        STDOUT.puts "----------------------------------------"
 
-      Process.run("sed -i 's#export ANDROID_SDK_ROOT=#{android_cl_linux_install_loc}##' ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
-      Process.run("sed -i 's#export ANDROID_HOME=#{android_cl_linux_install_loc}##' ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
-      Process.run("sed -i 's#export MOBSF_ADB_BINARY=#{android_cl_linux_install_loc}/platform-tools/adb##' ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
-      Process.run("sed -i 's#export PATH=$PATH:#{android_cl_linux_install_loc}/emulator:#{android_cl_linux_install_loc}/platform-tools:#{android_cl_linux_install_loc}/cmdline-tools/tools/bin##' ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        Process.run("sed -i 's#export ANDROID_SDK_ROOT=#{android_cl_mac_install_loc}##' ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        Process.run("sed -i 's#export ANDROID_HOME=#{android_cl_mac_install_loc}##' ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        Process.run("sed -i 's#export MOBSF_ADB_BINARY=#{android_cl_mac_install_loc}/platform-tools/adb##' ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        Process.run("sed -i 's#export PATH=$PATH:#{android_cl_mac_install_loc}/emulator:#{android_cl_mac_install_loc}/platform-tools:#{android_cl_mac_install_loc}/cmdline-tools/tools/bin##' ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
 
-      Process.run("echo 'export ANDROID_SDK_ROOT=#{android_cl_linux_install_loc}' >> ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
-      Process.run("echo 'export ANDROID_HOME=#{android_cl_linux_install_loc}' >> ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
-      Process.run("echo 'export MOBSF_ADB_BINARY=#{android_cl_linux_install_loc}/platform-tools/adb' >> ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        Process.run("echo 'export ANDROID_SDK_ROOT=#{android_cl_mac_install_loc}' >> ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        Process.run("echo 'export ANDROID_HOME=#{android_cl_mac_install_loc}' >> ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        Process.run("echo 'export MOBSF_ADB_BINARY=#{android_cl_mac_install_loc}/platform-tools/adb' >> ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
 
-      Process.run("echo 'export PATH=$PATH:#{android_cl_linux_install_loc}/emulator:#{android_cl_linux_install_loc}/platform-tools:#{android_cl_linux_install_loc}/cmdline-tools/tools/bin' >> ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        Process.run("echo 'export PATH=$PATH:#{android_cl_mac_install_loc}/emulator:#{android_cl_mac_install_loc}/platform-tools:#{android_cl_mac_install_loc}/cmdline-tools/tools/bin' >> ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
 
-      setEnvVarStatus = Process.run(%[. ~/.bashrc], shell: true, input: STDIN, output: STDOUT, error: STDERR)
-      raise Exception.new("Error installing MobSF depedencies : Setting Environment Variables") unless setEnvVarStatus.success?
+        setEnvVarStatus = Process.run(%[. ~/.bashrc], shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : Setting Environment Variables") unless setEnvVarStatus.success?
 
-      STDOUT.puts "----------------------------------------"
-      STDOUT.puts "Download Platform Specific Packages..."
-      STDOUT.puts "----------------------------------------"
+        STDOUT.puts "----------------------------------------"
+        STDOUT.puts "Download Platform Specific Packages..."
+        STDOUT.puts "----------------------------------------"
 
-      downloadAndroidPkgStatus = Process.run(%[sudo #{android_cl_linux_install_loc}/cmdline-tools/tools/bin/sdkmanager "platforms;android-28" "system-images;android-28;default;x86" "build-tools;28.0.3" ], shell: true, input: STDIN, output: STDOUT, error: STDERR)
-      raise Exception.new("Error installing MobSF depedencies : Download Platform Specific Packages") unless downloadAndroidPkgStatus.success?
+        downloadAndroidPkgStatus = Process.run(%[sudo #{android_cl_mac_install_loc}/cmdline-tools/tools/bin/sdkmanager "platforms;android-28" "system-images;android-28;default;x86" "build-tools;28.0.3" ], shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : Download Platform Specific Packages") unless downloadAndroidPkgStatus.success?
+      {% end %}
+
+      {% if flag?(:linux) %}
+        STDOUT.puts "------------------------------------"
+        STDOUT.puts "Fetching required packages..."
+        STDOUT.puts "------------------------------------"
+
+        fetchZipStatus = Process.run("sudo apt-get -y install zip unzip curl openjdk-11-jdk adb", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : zip") unless fetchZipStatus.success?
+
+        STDOUT.puts "------------------------------------"
+        STDOUT.puts "Setup Android Command Line Tool..."
+        STDOUT.puts "------------------------------------"
+
+        downloadAndroidCLStatus = Process.run("curl #{android_cl_linux_url} --output android_cl.zip", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : Download Android Command Line Tool") unless downloadAndroidCLStatus.success?
+
+        unzipAndroidCLStatus = Process.run("unzip android_cl.zip && sudo rm -rf #{android_cl_linux_install_loc}/cmdline-tools/ && sudo mkdir --parents #{android_cl_linux_install_loc}/cmdline-tools/ && mv cmdline-tools tools && sudo mv tools #{android_cl_linux_install_loc}/cmdline-tools/", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : Unzipping Android Command Line Tool") unless unzipAndroidCLStatus.success?
+
+        Process.run("rm android_cl.zip", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+
+        downloadSDKDepStatus = Process.run("sudo #{android_cl_linux_install_loc}/cmdline-tools/tools/bin/sdkmanager platform-tools emulator", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : Downloading platform-tools and emulator") unless downloadSDKDepStatus.success?
+
+        STDOUT.puts "----------------------------------------"
+        STDOUT.puts "Setting Environment Variables"
+        STDOUT.puts "----------------------------------------"
+
+        Process.run("sed -i 's#export ANDROID_SDK_ROOT=#{android_cl_linux_install_loc}##' ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        Process.run("sed -i 's#export ANDROID_HOME=#{android_cl_linux_install_loc}##' ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        Process.run("sed -i 's#export MOBSF_ADB_BINARY=#{android_cl_linux_install_loc}/platform-tools/adb##' ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        Process.run("sed -i 's#export PATH=$PATH:#{android_cl_linux_install_loc}/emulator:#{android_cl_linux_install_loc}/platform-tools:#{android_cl_linux_install_loc}/cmdline-tools/tools/bin##' ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+
+        Process.run("echo 'export ANDROID_SDK_ROOT=#{android_cl_linux_install_loc}' >> ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        Process.run("echo 'export ANDROID_HOME=#{android_cl_linux_install_loc}' >> ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        Process.run("echo 'export MOBSF_ADB_BINARY=#{android_cl_linux_install_loc}/platform-tools/adb' >> ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+
+        Process.run("echo 'export PATH=$PATH:#{android_cl_linux_install_loc}/emulator:#{android_cl_linux_install_loc}/platform-tools:#{android_cl_linux_install_loc}/cmdline-tools/tools/bin' >> ~/.bashrc", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+
+        setEnvVarStatus = Process.run(%[. ~/.bashrc], shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : Setting Environment Variables") unless setEnvVarStatus.success?
+
+        STDOUT.puts "----------------------------------------"
+        STDOUT.puts "Download Platform Specific Packages..."
+        STDOUT.puts "----------------------------------------"
+
+        downloadAndroidPkgStatus = Process.run(%[sudo #{android_cl_linux_install_loc}/cmdline-tools/tools/bin/sdkmanager "platforms;android-28" "system-images;android-28;default;x86" "build-tools;28.0.3" ], shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : Download Platform Specific Packages") unless downloadAndroidPkgStatus.success?
+      {% end %}
 
       STDOUT.puts "----------------------------------------"
       STDOUT.puts "Create a AVD device..."
--- src/mobsf-starter/install-mob-static-dep-command.cr
+++ src/mobsf-starter/install-mob-static-dep-command.cr
@@ -16,40 +16,81 @@ module MobSFStarter
     end
 
     def call(args : Array(String))
-      STDOUT.puts "------------------------------------"
-      STDOUT.puts "Updating Package List..."
-      STDOUT.puts "------------------------------------"
+      {% if flag?(:linux) %}
+        STDOUT.puts "------------------------------------"
+        STDOUT.puts "Updating Package List..."
+        STDOUT.puts "------------------------------------"
 
-      updateStatus = Process.run("sudo apt-get update", shell: true, input: STDIN, output: STDOUT, error: STDERR)
-      raise Exception.new("Error installing MobSF depedencies : Update") unless updateStatus.success?
+        updateStatus = Process.run("sudo apt-get update", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : Update") unless updateStatus.success?
 
-      STDOUT.puts "------------------------------------"
-      STDOUT.puts "Installing Git..."
-      STDOUT.puts "------------------------------------"
+        STDOUT.puts "------------------------------------"
+        STDOUT.puts "Installing Git..."
+        STDOUT.puts "------------------------------------"
 
-      gitStatus = Process.run("sudo apt-get -y install git", shell: true, input: STDIN, output: STDOUT, error: STDERR)
-      raise Exception.new("Error installing MobSF depedencies : Git") unless gitStatus.success?
+        gitStatus = Process.run("sudo apt-get -y install git", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : Git") unless gitStatus.success?
 
-      STDOUT.puts "------------------------------------"
-      STDOUT.puts "Installing Python 3..."
-      STDOUT.puts "------------------------------------"
+        STDOUT.puts "------------------------------------"
+        STDOUT.puts "Installing Python 3..."
+        STDOUT.puts "------------------------------------"
 
-      pythonStatus = Process.run("sudo apt-get -y install python3 python-is-python3", shell: true, input: STDIN, output: STDOUT, error: STDERR)
-      raise Exception.new("Error installing MobSF depedencies : Python 3") unless pythonStatus.success?
+        pythonStatus = Process.run("sudo apt-get -y install python3 python-is-python3", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : Python 3") unless pythonStatus.success?
 
-      STDOUT.puts "------------------------------------"
-      STDOUT.puts "Installing JDK 8..."
-      STDOUT.puts "------------------------------------"
+        STDOUT.puts "------------------------------------"
+        STDOUT.puts "Installing JDK 8..."
+        STDOUT.puts "------------------------------------"
 
-      jdkStatus = Process.run("sudo apt-get -y install openjdk-8-jdk", shell: true, input: STDIN, output: STDOUT, error: STDERR)
-      raise Exception.new("Error installing MobSF depedencies : JDK 8") unless jdkStatus.success?
+        jdkStatus = Process.run("sudo apt-get -y install openjdk-8-jdk", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : JDK 8") unless jdkStatus.success?
 
-      STDOUT.puts "------------------------------------"
-      STDOUT.puts "Installing Other Libraries..."
-      STDOUT.puts "------------------------------------"
+        STDOUT.puts "------------------------------------"
+        STDOUT.puts "Installing Other Libraries..."
+        STDOUT.puts "------------------------------------"
 
-      otherDepStatus = Process.run("sudo apt -y install python3-dev python3-venv python3-pip build-essential libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev wkhtmltopdf ", shell: true, input: STDIN, output: STDOUT, error: STDERR)
-      raise Exception.new("Error installing MobSF depedencies : Other dependencies") unless otherDepStatus.success?
+        otherDepStatus = Process.run("sudo apt -y install python3-dev python3-venv python3-pip build-essential libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev wkhtmltopdf ", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : Other dependencies") unless otherDepStatus.success?
+      {% end %}
+
+      {% if flag?(:darwin) %}
+        updateStatus = Process.run(%[/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"], shell: true, input: STDIN, output: STDOUT, error: STDERR)
+
+        STDOUT.puts "------------------------------------"
+        STDOUT.puts "Updating Package List..."
+        STDOUT.puts "------------------------------------"
+
+        updateStatus = Process.run("sudo brew update", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : Update") unless updateStatus.success?
+
+        STDOUT.puts "------------------------------------"
+        STDOUT.puts "Installing Git..."
+        STDOUT.puts "------------------------------------"
+
+        gitStatus = Process.run("sudo brew install git", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : Git") unless gitStatus.success?
+
+        STDOUT.puts "------------------------------------"
+        STDOUT.puts "Installing Python 3..."
+        STDOUT.puts "------------------------------------"
+
+        pythonStatus = Process.run("sudo brew install python3 python-is-python3", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : Python 3") unless pythonStatus.success?
+
+        STDOUT.puts "------------------------------------"
+        STDOUT.puts "Installing JDK 8..."
+        STDOUT.puts "------------------------------------"
+
+        jdkStatus = Process.run("sudo brew install openjdk@8", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : JDK 8") unless jdkStatus.success?
+
+        STDOUT.puts "------------------------------------"
+        STDOUT.puts "Installing Other Libraries..."
+        STDOUT.puts "------------------------------------"
+
+        otherDepStatus = Process.run("sudo brew install python3-dev python3-venv python3-pip build-essential libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev wkhtmltopdf ", shell: true, input: STDIN, output: STDOUT, error: STDERR)
+        raise Exception.new("Error installing MobSF depedencies : Other dependencies") unless otherDepStatus.success?
+      {% end %}
 
       STDOUT.puts "Dependencies for MobSF's Static Analysis installed!"
     end
